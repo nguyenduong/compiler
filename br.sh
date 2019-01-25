c490cdb813a0ca2a0d0539fdf2517c2394e9cfdc
Nguyen Thai Duong <duong.nguyen@gigatum.com>
2019-01-25 09:00:47 +0700

    update bash file

--- br.sh
+++ br.sh
@@ -1,5 +1,5 @@
 echo 'compile ATG'
-java -jar Coco.jar  xlang.ATG
+java -jar Coco.jar  $1
 
 echo 'compile java code'
 javac *.java -d ./output
@@ -7,7 +7,7 @@ javac *.java -d ./output
 echo 'test example'
 
 
-java -cp ./output JavaParser ex.x
+java -cp ./output JavaParser $2
 
 
 
